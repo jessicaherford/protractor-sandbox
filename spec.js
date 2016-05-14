e7a26d477422900d26aebf44e92068bf49a7c890
JessicaHerford <jrherford12@gmail.com>
2016-05-14 15:05:29 -0600

    Added history

--- spec.js
+++ spec.js
@@ -3,30 +3,46 @@ describe('Protractor Demo App', function() {
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+   firstNumber.sendKeys(a);
+   secondNumber.sendKeys(b);
+   goButton.click();
+ }
 
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
+  it('should have a history', function() {
+   add(1, 2);
+   add(3, 4);
 
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
 
-    goButton.click();
+   expect(history.last().getText()).toContain('1 + 2');
+   expect(history.first().getText()).toContain('3 + 4');
+ });
 
-    expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function() {
-    firstNumber.sendKeys(4);
-    secondNumber.sendKeys(6);
-
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('10');
-  });
+  // it('should have a title', function() {
+  //   expect(browser.getTitle()).toEqual('Super Calculator');
+  // });
+  //
+  // it('should add one and two', function() {
+  //   firstNumber.sendKeys(1);
+  //   secondNumber.sendKeys(2);
+  //
+  //   goButton.click();
+  //
+  //   expect(latestResult.getText()).toEqual('3');
+  // });
+  //
+  // it('should add four and six', function() {
+  //   firstNumber.sendKeys(4);
+  //   secondNumber.sendKeys(6);
+  //
+  //   goButton.click();
+  //
+  //   expect(latestResult.getText()).toEqual('10');
+  // });
 });
