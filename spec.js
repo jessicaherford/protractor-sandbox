5a82a7d6ae4e1abad42a98cd03bdf481fe52f01e
JessicaHerford <jrherford12@gmail.com>
2016-05-14 14:48:36 -0600

    added multiple browser testing capabilities

--- spec.js
+++ spec.js
@@ -1,12 +1,32 @@
 describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a title', function() {
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+
   it('should add one and two', function() {
-      browser.get('http://juliemr.github.io/protractor-demo/');
-      element(by.model('first')).sendKeys(1);
-      element(by.model('second')).sendKeys(2);
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
 
-      element(by.id('gobutton')).click();
+    goButton.click();
 
-      expect(element(by.binding('latest')).getText()).
-          toEqual('3'); 
-    });
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
+  });
 });
